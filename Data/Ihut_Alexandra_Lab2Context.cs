cb627e9c66def385b66b4b8c88b5fd9370cc595b
Ale <Ale@DESKTOP-2AN9UTF>
2022-10-26 15:10:50 +0300

    Add project files.

---
+++ Data/Ihut_Alexandra_Lab2Context.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Ihut_Alexandra_Lab2.Models;
+
+namespace Ihut_Alexandra_Lab2.Data
+{
+    public class Ihut_Alexandra_Lab2Context : DbContext
+    {
+        public Ihut_Alexandra_Lab2Context (DbContextOptions<Ihut_Alexandra_Lab2Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Ihut_Alexandra_Lab2.Models.Book> Book { get; set; } = default!;
+
+        public DbSet<Ihut_Alexandra_Lab2.Models.Publisher> Publisher { get; set; }
+    }
+}
